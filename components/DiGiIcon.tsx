9c2a4a601eab962b07adebf55037f3392c66516d
KimhabOrk <kimhab.ork@yahoo.com>
2024-03-22 00:42:45 +0700

    update

--- components/DiGiIcon.tsx
+++ components/DiGiIcon.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 export function DiGiIcon() {
   return (
-    <Image src='/digi3.svg' width={117} height={117} />
+    <Image src='/digi3.svg' width={117} height={117} alt="DiGiV3rse Icon" />
 
   )
 }
--- components/Icon.tsx
+++ components/Icon.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export function Icon() {
   return (
-      <Image src="/logo.png" width={1200} height={1200} />
+      <Image src="/logo.png" width={1200} height={1200} alt="DiGiV3rse Logo" />
   )
 }
