cd10a8c6eb21a499952e2b0ea16b338b4ee7d14e
KimhabOrk <kimhab.ork@yahoo.com>
2024-03-22 00:38:06 +0700

    update

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,20 +1,7 @@
-export function Icon({
-  fill,
-  ...props
-}) {
+import Image from 'next/image'
+
+export function Icon() {
   return (
-    <svg {...props} version="1.1" viewBox="0 0 1200 1200" xmlns="http://www.w3.org/2000/svg">
-      <g>
-        <path d="m636 432h108v108h-108z" fill={fill} />
-        <path d="m420 648h108v108h-108z" fill={fill} />
-        <path d="m312 756h108v108h-108z" fill={fill} />
-        <path d="m744 324h108v108h-108z" fill={fill} />
-        <path d="m420 432h108v108h-108z" fill={fill} />
-        <path d="m528 540h108v108h-108z" fill={fill} />
-        <path d="m636 648h108v108h-108z" fill={fill} />
-        <path d="m744 756h108v108h-108z" fill={fill} />
-        <path d="m312 324h108v108h-108z" fill={fill} />
-      </g>
-    </svg>
+      <Image src="/logo.png" width={1200} height={1200} alt="DiGiV3rse icon" />
   )
 }
